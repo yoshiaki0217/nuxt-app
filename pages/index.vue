e3df6d299b732a86551ada0fe62c9b27fd02efb7
大城由晃 <yoshiaki19980217@icloud.com>
2021-06-18 20:28:54 +0900

    firstpush

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title text-2xl m-96">nuxt-web</h1>
+      <h1 class="title text-2xl m-2000">nuxt-web</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        96: '24rem',
+        2000: '32rem',
+      },
+    },
   },
   variants: {
     extend: {},

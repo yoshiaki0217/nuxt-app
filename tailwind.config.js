4340835a9469d90c9cbf665595214e9da66bd192
大城由晃 <yoshiaki19980217@icloud.com>
2021-06-18 19:35:07 +0900

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      margin: {
+        96: '24rem',
+        128: '32rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

4920c884942da4d48108d771018e4c9b8fd66dab
大城由晃 <yoshiaki19980217@icloud.com>
2021-06-18 19:52:45 +0900

    tailwind修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      margin: {
-        96: '24rem',
-        128: '32rem',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
